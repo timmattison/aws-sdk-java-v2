547a47abfb33ad8fd511ffe85aa8b06133f54c28
Matthew Miller <millem@amazon.com>
2020-11-13 11:03:10 -0800

    Updated benchmarks

--- test/sdk-benchmarks/src/main/java/software/amazon/awssdk/benchmark/HttpClientComparison.java
+++ test/sdk-benchmarks/src/main/java/software/amazon/awssdk/benchmark/HttpClientComparison.java
@@ -17,9 +17,12 @@ package software.amazon.awssdk.benchmark;
 
 import static java.util.Collections.singletonMap;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
@@ -31,6 +34,7 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import software.amazon.awssdk.core.client.config.SdkAdvancedAsyncClientOption;
+import software.amazon.awssdk.core.retry.RetryPolicy;
 import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.http.crt.AwsCrtAsyncHttpClient;
 import software.amazon.awssdk.http.nio.netty.NettyNioAsyncHttpClient;
@@ -38,31 +42,13 @@ import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
-import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
 import software.amazon.awssdk.utils.Either;
 
 @State(Scope.Benchmark)
+@Measurement(iterations = 5, time = 30, timeUnit = TimeUnit.SECONDS)
 @BenchmarkMode({Mode.Throughput, Mode.SampleTime})
 public class HttpClientComparison {
-    private static final PutItemRequest PUT_ITEM_REQUEST = PutItemRequest.builder()
-                                                                         .tableName("millem-throughput")
-                                                                         .item(singletonMap("key", AttributeValue.builder()
-                                                                                                                 .s("value")
-                                                                                                                 .build()))
-                                                                         .build();
-
-    @Benchmark
-    @Threads(Threads.MAX)
-    public void callsCpuCount(Blackhole blackhole, BenchmarkState state) {
-        call(blackhole, state);
-    }
-
-    @Benchmark
-    @Threads(1)
-    public void calls1(Blackhole blackhole, BenchmarkState state) {
-        call(blackhole, state);
-    }
-
     @Benchmark
     @Threads(10)
     public void calls10(Blackhole blackhole, BenchmarkState state) {
@@ -76,13 +62,21 @@ public class HttpClientComparison {
     }
 
     private void call(Blackhole blackhole, BenchmarkState state) {
-        state.clientInstance.apply(sync -> blackhole.consume(sync.putItem(PUT_ITEM_REQUEST)),
-                                   async -> blackhole.consume(async.putItem(PUT_ITEM_REQUEST).join()));
+        state.clientInstance.apply(sync -> blackhole.consume(sync.getItem(getItemRequest())),
+                                   async -> blackhole.consume(async.getItem(getItemRequest()).join()));
+    }
+
+    private GetItemRequest getItemRequest() {
+        String key = "value" + ThreadLocalRandom.current().nextInt(0, 100);
+        return GetItemRequest.builder()
+                             .tableName("millem-throughput")
+                             .key(singletonMap("key", AttributeValue.builder().s(key).build()))
+                             .build();
     }
 
     @State(Scope.Benchmark)
     public static class BenchmarkState {
-        @Param({"netty", "crt"})
+        @Param({"apache", "urlConnection", "netty", "crt"})
         public String clientName;
 
         public Either<DynamoDbClient, DynamoDbAsyncClient> clientInstance;
@@ -91,21 +85,29 @@ public class HttpClientComparison {
         public void setUp() {
             switch (clientName) {
                 case "apache":
-                    clientInstance = Either.left(DynamoDbClient.builder().httpClientBuilder(ApacheHttpClient.builder().maxConnections(100)).build());
+                    clientInstance = Either.left(DynamoDbClient.builder()
+                                                               .httpClientBuilder(ApacheHttpClient.builder().maxConnections(100))
+                                                               .overrideConfiguration(c -> c.retryPolicy(RetryPolicy.none()))
+                                                               .build());
                     break;
                 case "urlConnection":
-                    clientInstance = Either.left(DynamoDbClient.builder().httpClientBuilder(UrlConnectionHttpClient.builder()).build());
+                    clientInstance = Either.left(DynamoDbClient.builder()
+                                                               .httpClientBuilder(UrlConnectionHttpClient.builder())
+                                                               .overrideConfiguration(c -> c.retryPolicy(RetryPolicy.none()))
+                                                               .build());
                     break;
                 case "netty":
                     clientInstance = Either.right(DynamoDbAsyncClient.builder()
                                                                      .asyncConfiguration(c -> c.advancedOption(SdkAdvancedAsyncClientOption.FUTURE_COMPLETION_EXECUTOR, Runnable::run))
                                                                      .httpClientBuilder(NettyNioAsyncHttpClient.builder().maxConcurrency(100))
+                                                                     .overrideConfiguration(c -> c.retryPolicy(RetryPolicy.none()))
                                                                      .build());
                     break;
                 case "crt":
                     clientInstance = Either.right(DynamoDbAsyncClient.builder()
                                                                      .asyncConfiguration(c -> c.advancedOption(SdkAdvancedAsyncClientOption.FUTURE_COMPLETION_EXECUTOR, Runnable::run))
                                                                      .httpClientBuilder(AwsCrtAsyncHttpClient.builder().maxConcurrency(100))
+                                                                     .overrideConfiguration(c -> c.retryPolicy(RetryPolicy.none()))
                                                                      .build());
                     break;
                 default:
@@ -115,6 +117,14 @@ public class HttpClientComparison {
     }
 
     public static void main(String... args) throws Exception {
+        try (DynamoDbClient client = DynamoDbClient.builder().httpClientBuilder(ApacheHttpClient.builder()).build()) {
+            for (int i = 0; i < 100; ++i) {
+                String keyName = "value" + i;
+                client.putItem(r -> r.tableName("millem-throughput")
+                                     .item(singletonMap("key", AttributeValue.builder().s(keyName).build())));
+            }
+        }
+
         Options opt = new OptionsBuilder()
             .include(HttpClientComparison.class.getSimpleName())
             .build();
